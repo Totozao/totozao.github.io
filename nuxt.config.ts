033f86a9ffd6b0365fdd4ba1924eca40cdb0aea7
Yurii Krupenyk <totozao.totozao@yandex.ru>
2026-04-16 20:08:46 +0300

    feat: tries into pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,5 +39,42 @@ export default defineNuxtConfig({
     "@pinia/nuxt",
     "pinia-plugin-persistedstate",
     "@nuxt/eslint",
+    "@vite-pwa/nuxt",
   ],
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Ведущая мафия",
+      short_name: "МАФО",
+      description: "Мафия для ведущего",
+      theme_color: "#4A90E2",
+      icons: [
+        {
+          src: "/pwa-icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/pwa-icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: "https://example.com/.*",
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api-cache",
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 86400,
+            },
+          },
+        },
+      ],
+    },
+  },
 });
